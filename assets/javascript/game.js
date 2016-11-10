9f7106123448d08d70714674b7d4361844249927
drichard1989 <drichard1989@icloud.com>
2016-11-09 18:14:55 -0600

    I psuedo-coded my JS, and am pushing it for Tish to see. ;)

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// - First, we need to create all of the variables we think we are going to need. 
+// - Then, we need to create all of the functions we think we are going to need. 
+// 	-Function for the start of the game, called reset.
+// 		-This will do a series of random calculations. 
+// 			- First, randomly calculate a randomGoalNumber for the user to try and get to.
+// 				- This number must be between 19 and 120, and will be visible.
+// 			- Then, the computer will also randomly calclulate random numbers for each of the 4 images. 
+// 				- rubyNumber, emeraldNumber, sapphireNumber, topazNumber.
+// 					- These numbers must be between 1 and 12, and not be visible.
+// 	- Then, we will create a function that will run when an image is clicked.
+// 		- When any of the 4 buttons are clicked, they are added together in the yourTotalScore area. 
+// 		- The value will be added in the display for yourTotalScore.
+// 			- If yourTotalScore is larger than the randomGoalNumber, you lose, game resets, and adds a loss.  
+// 			- If yourTotalScore is = to the randomGoalNumber, you win, and reset function runs after adding a win.
+
+// - Then, write your HTML inline? code to plug in the variables on the DOM, and hopefully the thing starts to work!
+
